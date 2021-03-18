27faa54eb1ed27c8927ae8b15284166fa1bcbc49
kblaine <k12blaine@gmail.com>
2021-03-18 14:30:37 -0400

    Update

---
+++ Development/03-20-2021_Homework.js
@@ -0,0 +1,44 @@
+/* 1. Pick a cat from Wikipedia's List of Fictional Felines "https://en.wikipedia.org/wiki/List_of_fictional_felines"
+      Create an object named mycat with properties that represent the information listed in each column on that Wikipedia page.
+      (for example: the character's name, origin, and author).
+*/ Crookshanks/Hermione/Hermiones pet/Jk Rowling
+
+
+
+
+
+/* 2. Write another line of code that adds a new property to your penguin called canRoar and set it to true or false based on the type of animal.
+     (Note: Don't modify your penguin-creation code that you wrote above! Do this step in a separate line of code.)
+*/
+
+canRoar: F
+
+
+
+/* 3. Add a method to your penguin called speak() that prints "Rooaaar" or "I can't roar. I can only meow. :3" based on the can Roar property.
+    (Note: Again, don't modify your previous code! Do this step by writing a new line of code.)
+*/
+
+Speak(I can only meow)
+
+
+
+/* 4. Recreate the following array by defining a Book constructor and creating 3 book objects from it:
+var library = [
+   {                                                                               {
+       author: 'Bill Gates',                                                        author:'JK. Rowling',
+       title: 'The Road Ahead',                                                      Title: 'Harry Potter',
+       readingStatus: true                                                            readingstatus: Fiction
+
+   },                                                                               },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games',
+       readingStatus: false
+   }];
+*/
