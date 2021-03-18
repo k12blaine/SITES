16e9eeab6b3e5400cdf7ce6deab5ab4ea38ed161
kblaine <k12blaine@gmail.com>
2021-03-18 16:46:01 -0400

    Final

--- Development/03-20-2021_Homework-Answers.js
+++ Development/03-20-2021_Homework-Answers.js
@@ -2,6 +2,11 @@
       Create an object below named myMovie with properties that represent the information (title, genre, year).
 
 */
+    var myMovie = {
+      title: 'Colombiana',
+      genre: 'Action',
+      year: 2011
+    }
 
 
 
@@ -11,7 +16,7 @@
      (Note: Don't modify your movie creation code that you wrote above! Do this step in a separate line of code.)
      *Remember, true or false are boolean data types
 */
-
+      netflix: false;
 
 
 
@@ -19,14 +24,13 @@
       playing on Netflix" or "The movie is unavailable on Netflix" based on the value of the netflix property.
     (Note: Again, don't modify your previous code! Do this step by writing a new line of code.)
 */
-
 //define method
-
-
+  playMovie : function() {
+    console.log("The movie is unavailable on Netflix")
+  }
 
 //call method
-
-
+    mymovie.playMovie()
 
 /* 4. Recreate the following array by defining a Book constructor and creating 3 book objects from it (book1, book2, and book3),
       and then adding them to array library:
@@ -34,7 +38,7 @@ var library = [
    {                                                                               {
        author: 'Bill Gates',                                                        author:'JK. Rowling',
        title: 'The Road Ahead',                                                      Title: 'Harry Potter',
-       readingStatus: true                                                            readingstatus: false
+       readingStatus: true                                                            readingstatus: Fiction
 
    },                                                                               },
    {
@@ -50,14 +54,18 @@ var library = [
 */
 
 //Define object constructor
+function Book(author, title, readingStatus)
+{
+  this.author = author;
+  this.title = title;
+  this.readingStatus = readingStatus;
 
-
-
-
-
+}
 
 // 3function calls
-
-
+var book1 = newBook("Bill Gates", "The Road Ahead", true)
+var book2 = newBook("Bill Gates", "The Road Ahead", true)
+var book3 = newBook("Bill Gates", "The Road Ahead", true)
 
 //create array
+var library = [book1, book2, book3];
